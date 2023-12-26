12ecc4775dc2da946e46d92d9b98fbdc889230b6
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-12-26 17:04:19 +0545

    Added a basic hangman figure (refine needed)

--- hangman/src/app/page.js
+++ hangman/src/app/page.js
@@ -3,13 +3,15 @@
 import hangmanWords from "@/constants/words";
 import alphabets from "@/constants/alphabets";
 import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
+import hangmanSteps from "@/constants/hangman";
 
 const Home = () => {
   // Number of attempts allowed
-  const attemptsLimit = 10;
+  const attemptsLimit = 6;
 
   // State to keep track of the word which is given as a question
-  const [questionWord, setQuestionWord] = useState(null);
+  const [questionWord, setQuestionWord] = useState("");
 
   // To keep track of which words to show
   const [wordIndexesToShow, setWordIndexesToShow] = useState([0]);
@@ -17,18 +19,27 @@ const Home = () => {
   // Number of wrong attempts
   const [wrongAttempts, setWrongAttempts] = useState(0);
 
+  // State to track clicked words
+  const [clickedButtons, setClickedButtons] = useState([]);
+
   // To keep track of things loading in our application
   const [isLoading, setIsLoading] = useState(true);
+
+  // Win state
+  const [isWin, setIsWin] = useState(false);
 
   // Loosing and winning state
   const [isGameOver, setIsGameOver] = useState(false);
 
   // Function that handles guesses (when user clicks a word)
   const handleGuess = (e) => {
-    console.log("WORD ", questionWord?.word);
+    const { value } = e.target;
+
+    // Update the state to include the button so that next time it grays out
+    setClickedButtons((prevClickedButtons) => [...prevClickedButtons, value]);
 
     // If the guessed word is not included in the given word, just do nothing and add wrong attempts count
-    if (!questionWord?.word.split("").includes(e.target.value)) {
+    if (!questionWord?.word.split("").includes(value)) {
       return setWrongAttempts((currAttempts) => currAttempts + 1);
     }
 
@@ -42,32 +53,82 @@ const Home = () => {
     setWordIndexesToShow([...wordIndexesToShow, ...indexes]);
   };
 
-  useEffect(() => {
+  // Restart the game
+  const restart = () => {
+    setWrongAttempts(0);
+    setIsGameOver(false);
+    setClickedButtons([]);
+    setIsWin(false);
+    setWordIndexesToShow([0]);
+    loadQuestion();
+  };
+
+  // Load a question
+  const loadQuestion = () => {
     const randomIndex = Math.floor(Math.random() * hangmanWords.length);
     const randomWordObject = hangmanWords[randomIndex];
     setQuestionWord(randomWordObject);
+  };
+
+  // Load a word
+  useEffect(() => {
+    loadQuestion();
     setIsLoading(false);
   }, []);
 
   // For checking if game is over
   useEffect(() => {
-    console.log(wrongAttempts, attemptsLimit);
     setIsGameOver(wrongAttempts === attemptsLimit);
   }, [wrongAttempts]);
+
+  // Check if win
+  useEffect(() => {
+    setIsWin(questionWord.word?.split("").length === wordIndexesToShow.length);
+  }, [wordIndexesToShow, questionWord]);
 
   return (
     <div className="bg-slate-800 min-h-screen flex justify-center items-center">
       <div className="bg-white container mx-auto rounded-xl flex items-center center flex-wrap flex-col xl:flex-row gap-12 p-4 xl:p-12">
         {/* Left Layout */}
-        <div className="bg-blue-400 h-96 w-full xl:flex-1 flex justify-center items-center rounded-md">
-          <h1 className="text-2xl font-semibold tracking-tight text-white">
-            HANGMAN COMING SOON
-          </h1>
+        <div className="bg-blue-400 min-h-[60vh] w-full xl:flex-1 flex justify-center items-center rounded-md">
+          <textarea
+            className="bg-transparent text-white font-bold text-4xl"
+            rows={10}
+            value={hangmanSteps[wrongAttempts]}
+          />
         </div>
 
         {/* Right layout */}
         {isGameOver ? (
-          <div>GAME OVER</div>
+          <div className="flex-1 gap-4 flex justify-center items-center flex-col overflow-hidden">
+            <span className="font-extrabold text-6xl text-gray-400">
+              GAME OVER
+            </span>
+            <button
+              className="bg-blue-400 hover:scale-105 py-2 px-4 text-lg rounded-md text-white uppercase font-semibold"
+              onClick={restart}
+            >
+              Restart
+            </button>
+          </div>
+        ) : isWin ? (
+          <div className="flex-1 gap-4 flex justify-center items-center flex-col">
+            <Confetti
+              numberOfPieces={500}
+              opacity={0.6}
+              friction={0.99}
+              tweenDuration={5000}
+            />
+            <span className="font-extrabold text-4xl text-gray-400">
+              Congratulations! You did it 🥳
+            </span>
+            <button
+              className="bg-blue-400 hover:scale-105 py-2 px-4 text-lg rounded-md text-white uppercase font-semibold"
+              onClick={restart}
+            >
+              Replay
+            </button>
+          </div>
         ) : (
           <div className="flex flex-col gap-4 xl:flex-1 items-center justify-center">
             {/* Word input */}
@@ -100,7 +161,7 @@ const Home = () => {
               p
               className="text-sm text-red-400 text-center font-medium tracking-wide"
             >
-              Incorrect guess: {wrongAttempts}/{attemptsLimit}
+              Incorrect attempt/s: {wrongAttempts}/{attemptsLimit}
               {attemptsLimit - wrongAttempts < 4 && (
                 <span className="block">
                   Be careful! You only have {attemptsLimit - wrongAttempts}{" "}
@@ -110,11 +171,16 @@ const Home = () => {
             </p>
 
             {/* Keyboard */}
-            <div className="border border-gray-200 p-2 flex items-center rounded-md gap-2 justify-start flex-wrap">
+            <div className="border border-gray-200 p-2 flex items-center rounded-md gap-2 flex-wrap">
               {alphabets.map((alphabet) => (
                 <button
-                  className={`bg-blue-400 min-h-12 min-w-12 flex justify-center items-center text-white cursor-pointer hover:scale-105 rounded-md`}
+                  className={`min-h-12 min-w-12 flex justify-center items-center text-white hover:scale-105 rounded-md ${
+                    clickedButtons.includes(alphabet)
+                      ? "bg-gray-300 cursor-not-allowed"
+                      : "cursor-pointer bg-blue-400"
+                  }`}
                   value={alphabet}
+                  disabled={clickedButtons.includes(alphabet)}
                   onClick={handleGuess}
                 >
                   {alphabet.toUpperCase()}
