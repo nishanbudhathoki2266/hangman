4755fd4f8c79f899634f4f8acfb93203da8a7113
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-12-26 10:10:13 +0545

    Added basic states to load a question

--- hangman/src/app/page.js
+++ hangman/src/app/page.js
@@ -1,7 +1,91 @@
-import React from "react";
+"use client";
+
+import hangmanWords from "@/constants/words";
+import alphabets from "@/constants/alphabets";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  return <h1>Hangman</h1>;
+  // State to keep track of the word which is given as a question
+  const [questionWord, setQuestionWord] = useState(null);
+
+  // To keep track of which words to show
+  const [wordIndexesToShow, setWordIndexesToShow] = useState([]);
+
+  // To keep track of things loading in our application
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * hangmanWords.length);
+    const randomWordObject = hangmanWords[randomIndex];
+    setQuestionWord(randomWordObject);
+    setWordIndexesToShow([
+      ...wordIndexesToShow,
+      0,
+      Math.floor(randomWordObject?.word?.split("").length / 2),
+    ]);
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <div className="bg-slate-800 min-h-screen flex justify-center items-center">
+      <div className="bg-white container mx-auto rounded-xl flex items-center center flex-wrap flex-col xl:flex-row gap-12 p-4 xl:p-12">
+        {/* Left Layout */}
+        <div className="bg-blue-400 h-96 w-full xl:flex-1 flex justify-center items-center rounded-md">
+          <h1 className="text-2xl font-semibold tracking-tight text-white">
+            HANGMAN COMING SOON
+          </h1>
+        </div>
+
+        {/* Right layout */}
+        <div className="flex flex-col gap-4 xl:flex-1 items-center justify-center">
+          {/* Word input */}
+          {isLoading ? (
+            <p className="text-sm font-medium tracking-wide">
+              Finding a word for you...
+            </p>
+          ) : (
+            <div className="flex items-center justify-center flex-col gap-2">
+              <p className="flex items-center gap-2">
+                {questionWord?.word.split("").map((word, index) => (
+                  <span className="font-bold">
+                    {wordIndexesToShow.includes(index)
+                      ? index === 0
+                        ? word.toUpperCase()
+                        : word
+                      : "___"}
+                  </span>
+                ))}
+              </p>
+              <p className="text-sm font-medium tracking-wide">
+                Hint:
+                <span className="font-semibold">
+                  {questionWord?.description}
+                </span>
+              </p>
+            </div>
+          )}
+
+          {/* Guess indicator */}
+          <p p className="text-sm text-red-400 font-medium tracking-wide">
+            Incorrect guess: 1/5
+          </p>
+
+          {/* Keyboard */}
+          <div className="border border-gray-200 p-2 flex items-center rounded-md gap-2 justify-start flex-wrap">
+            {alphabets.map((alphabet) => (
+              <button
+                className="bg-blue-400 min-h-12 min-w-12 flex justify-center items-center text-white cursor-pointer hover:scale-105 rounded-md"
+                value={alphabet}
+                onClick={(e) => console.log(e.target.value)}
+              >
+                {alphabet.toUpperCase()}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ hangman/src/constants/words.js
@@ -0,0 +1,81 @@
+const hangmanWords = [
+  { word: "apple", description: "A red or green fruit with a core and seeds" },
+  { word: "banana", description: "A yellow fruit with a peel" },
+  { word: "computer", description: "An electronic device for processing data" },
+  { word: "giraffe", description: "A tall, long-necked African mammal" },
+  { word: "ocean", description: "A large body of saltwater" },
+  {
+    word: "umbrella",
+    description: "A device used to protect from rain or sun",
+  },
+  {
+    word: "mountain",
+    description:
+      "A large landform that rises prominently above its surroundings",
+  },
+  {
+    word: "jazz",
+    description: "A genre of music with improvisation and syncopated rhythms",
+  },
+  { word: "hiking", description: "An outdoor activity of walking in nature" },
+  {
+    word: "sunflower",
+    description: "A tall plant with a large, yellow flower head",
+  },
+  { word: "whale", description: "A large marine mammal" },
+  { word: "bicycle", description: "A two-wheeled vehicle powered by pedaling" },
+  {
+    word: "chocolate",
+    description: "A sweet, brown food made from roasted and ground cacao seeds",
+  },
+  {
+    word: "telescope",
+    description: "An optical instrument for observing distant objects",
+  },
+  { word: "oxygen", description: "A chemical element essential for life" },
+  {
+    word: "quasar",
+    description: "An extremely bright and distant celestial object",
+  },
+  {
+    word: "lighthouse",
+    description: "A tall structure with a light for navigation",
+  },
+  {
+    word: "kangaroo",
+    description: "A marsupial native to Australia with a distinctive pouch",
+  },
+  {
+    word: "library",
+    description:
+      "A place where books and other materials are available for borrowing or reading",
+  },
+  {
+    word: "dragonfly",
+    description:
+      "An insect with large, multifaceted eyes and four transparent wings",
+  },
+  {
+    word: "elephant",
+    description: "A large mammal with tusks and a long trunk",
+  },
+  {
+    word: "pyramid",
+    description:
+      "A structure with a square or triangular base and four triangular sides",
+  },
+  {
+    word: "astronomy",
+    description: "The scientific study of celestial objects and phenomena",
+  },
+  {
+    word: "volcano",
+    description: "A mountain that erupts with lava, ash, and gases",
+  },
+  {
+    word: "penguin",
+    description: "A flightless bird native to the Southern Hemisphere",
+  },
+];
+
+export default hangmanWords;
